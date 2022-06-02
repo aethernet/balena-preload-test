c03fafd635819b778c7a4380f6a58059c11a6bcb
JOASSART Edwin <edwin.joassart@ext.europarl.europa.eu>
2022-06-02 10:01:09 +0200

    get images id from apps.json & repositories.json

--- extractApp.mjs
+++ extractApp.mjs
@@ -2,30 +2,23 @@
 
 const inPath = path.join(__dirname, "in")
 const outPath = path.join(__dirname, "out")
-const apps = await fs.readJson(path.join(__dirname, "in", "apps.json"))
+const apps = await fs.readJson(path.join(inPath, "apps.json"))
 
-console.log(apps)
-
+/** Extract images name/tag from apps.json */
 const appId = Object.keys(apps.apps)[0]
 const images = Object.keys(apps.apps[appId].services)
   .map(key => apps.apps[appId].services[key].image)
 
-/** 
- * FIXME : Here 
- * Here we would loop over `images` and do `docker inspect _iamge_`  for each of those, 
- * as we don't have access to the engine in this env; we'll just simulate that manually
- * 
-*/
+/** Get image id for image tag using `repositories.json` */
+const repositories = await fs.readJson(path.join(inPath, 'var', 'lib', 'docker', 'image', 'overlay2', 'repositories.json'))
+
+const imagesId = images
+  .map(image => repositories.Repositories[image.split('@')[0].split(':')[0]][image].split(':')[1])
 
 // clean out folder
 await $`rm -rf ${outPath}`
 
 /** Run extract for each image in the app */
-const imagesId = [
-  'eade27e71cefb1067d7fd15d5d754ab9fb31471e9478068cff97b8e530efdd35',
-  'bfe7e8cf4d58cb61701dd110ee0560f3b64bbc5c09043c8529c8841a19533363'
-]
-
 for(let image of imagesId) {
   await $`./extract.mjs --image ${image}`
 }
