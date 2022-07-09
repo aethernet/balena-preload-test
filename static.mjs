092eb1ca7152eb96e574ea40c9535b385366fd0e
JOASSART Edwin <edwin.joassart@ext.europarl.europa.eu>
2022-07-09 19:52:17 +0200

    with better perms

--- static.mjs
+++ static.mjs
@@ -12,7 +12,7 @@ const token = argv.token ?? (await $`cat < ./api_key`)
 // utilities
 // FIXME: there's no mechanism to prevent collision
 const generateLinkId = () => [...Array(26).keys()].map(() => "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * 34)]).join("")
-const generateCacheId = () => [...Array(32).keys()].map(() => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 34)]).join("")
+const generateCacheId = () => [...Array(64).keys()].map(() => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 34)]).join("")
 
 const baseOutPath = path.join(__dirname, "out", "docker")
 const baseInPath = path.join(__dirname, "in", "images", imageUrl.split("/").reverse()[0])
@@ -42,7 +42,7 @@ const tgzLayersDigest = manifestJson.layers.map((layer) => ({
 // sha256sum to get the digest
 // then
 // compute each layers chainid from the diffids
-// formula is : sha256 of a string composed of the diffId of the layer, a space, and the pathId of the parent layer
+// formula is : sha256 of a string composed of the chainid of the parent layer, a space, and the diffid of the layer
 // the topmost layer (the first one), has no parent so its chainid = its diffid
 // also
 // keeping the size of the layer handy for future use
@@ -63,18 +63,21 @@ const digests = await Promise.all(
       linkid: generateLinkId(),
     }
   })
-).then((digests) =>
-  digests.map((digest, key, digests) => ({
-    ...digest,
-    chainid:
-      key === 0
-        ? digest.layerid
+).then((digests) => {
+  const newDigests = []
+  for (const key in digests) {
+    newDigests.push({
+      ...digests[key],
+      chainid: key == 0 
+        ? digests[key].layerid
         : crypto
             .createHash("sha256")
-            .update(`sha256:${digests[key - 1].layerid} sha256:${digest.layerid}`)
+            .update(`sha256:${newDigests[key - 1].chainid} sha256:${digests[key].layerid}`)
             .digest("hex"),
-  }))
-)
+    })
+  }
+  return newDigests
+})
 
 // prebuild a chain of linkid path to use in overlays2's `lower` files
 const linkIdFullChain = digests.map((digest) => `l/${digest.linkid}`)
@@ -85,12 +88,17 @@ await $`mkdir -p ${path.join(baseOutPath, "image", "overlay2", "imagedb", "metad
 await $`mkdir -p ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256")}`
 await $`mkdir -p ${path.join(baseOutPath, "overlay2", "l")}`
 
+await $`chmod 700 ${path.join(baseOutPath, "image", "overlay2", "imagedb", "content", "sha256")}`
+await $`chmod 700 ${path.join(baseOutPath, "image", "overlay2", "imagedb", "metadata", "sha256")}`
+await $`chmod 700 ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256")}`
+await $`chmod 700 ${path.join(baseOutPath, "overlay2", "l")}`
+
 // images/overlay2/imagedb
 // ./content/sha256 => images json
 // ./metadata/sha256/*imageId*/lastUpdated => isoString date
-$`cp ${path.join(baseInPath, imageHash)} ${path.join(baseOutPath, "image", "overlay2", "imagedb", "content", "sha256", imageHash)}`
+await $`cp ${path.join(baseInPath, imageHash)} ${path.join(baseOutPath, "image", "overlay2", "imagedb", "content", "sha256", imageHash)}`
 await $`mkdir -p ${path.join(baseOutPath, "image", "overlay2", "imagedb", "metadata", "sha256", imageHash)}`
-$`echo ${new Date().toISOString()} > ${path.join(baseOutPath, "image", "overlay2", "imagedb", "metadata", "sha256", imageHash, "lastUpdated")}`
+await $`echo ${new Date().toISOString()} > ${path.join(baseOutPath, "image", "overlay2", "imagedb", "metadata", "sha256", imageHash, "lastUpdated")}`
 
 // images/overlay2/layerdb/sha256/*chainId*
 // ./cache-id => id of corresponding overlay2 -> 32 random alphanum char
@@ -104,27 +112,41 @@ $`echo ${new Date().toISOString()} > ${path.join(baseOutPath, "image", "overlay2
 // <- my best guess is a digest of the `diff` folder but i don't know how.
 // ./commited => empty file, not sure what its role is
 // ./link => linkid (random 24 caps alphanum char) / related to the l symlinks
-// ./lower => chain of lower layers links path such as : `l/*LOWER1LINK*:l/*LOWER2LINK*:l/...`, if it's the first in the chain it has all lowers, if it's last it has none // TODO: not sure i'm computing this one right
 // ./diff/* => actual content of the layer (gunzip + untar of the layer archive)
 // ./work => empty folder
+
+// ./lower => chain of lower layers links path such as : `l/*LOWER1LINK*:l/*LOWER2LINK*:l/...`, if it's the first in the chain it has all lowers, if it's last it has no lower file
+
 // overlay2/l/*linkid* -> symlink pointing to related overlay diff folder; linkid has to be the same as content of link file
 
 for (const key in digests) {
   const { layerid, chainid, gzipid, cacheid, size, linkid } = digests[key]
   await $`mkdir -p ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid)}`
-  $`echo ${`sha256:${cacheid}`} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "cache-id")}`
-  $`echo ${`sha256:${layerid}`} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "diff")}`
-  $`echo ${size} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "size")}`
-  if (key > 0)
-    $`echo ${`sha256:${digests[key - 1].chainid}`} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "parent")}`
+  await $`echo ${cacheid} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "cache-id")}`
+  await $`echo ${`sha256:${layerid}`} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "diff")}`
+  await $`echo ${size} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "size")}`
+  if (key > 0) {
+    await $`echo ${`sha256:${digests[key - 1].chainid}`} > ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid, "parent")}`
+  }
   
+  await $`chmod -R 744 ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid)}`
+  await $`chmod 700 ${path.join(baseOutPath, "image", "overlay2", "layerdb", "sha256", chainid)}`
+
   await $`mkdir -p ${path.join(baseOutPath, "overlay2", cacheid, "work")}`
-  $`touch ${path.join(baseOutPath, "overlay2", cacheid, "commited")}`
-  $`echo ${linkid} > ${path.join(baseOutPath, "overlay2", cacheid, "link")}`
+  await $`chmod 700 ${path.join(baseOutPath, "overlay2", cacheid)}`
+  await $`touch ${path.join(baseOutPath, "overlay2", cacheid, "commited")}`
+  await $`chmod 600 ${path.join(baseOutPath, "overlay2", cacheid, "commited")}`
+  await $`echo ${linkid} > ${path.join(baseOutPath, "overlay2", cacheid, "link")}`
+  await $`chmod 644 ${path.join(baseOutPath, "overlay2", cacheid, "link")}`
   await $`mkdir -p ${path.join(baseOutPath, "overlay2", cacheid, "diff")}`
-  $`tar -zxf ${path.join(baseInPath, `${gzipid}.tar`)} -C ${path.join(baseOutPath, "overlay2", cacheid, "diff")}`
-  $`echo ${linkIdFullChain.slice(key).join(":")} > ${path.join(baseOutPath, "overlay2", cacheid, "lower")}`
-  $`ln -s ${path.join(baseOutPath, "overlay2", cacheid, "diff")} ${path.join(baseOutPath, "overlay2", "l", linkid)}`
+  await $`tar -zxf ${path.join(baseInPath, `${gzipid}.tar`)} -C ${path.join(baseOutPath, "overlay2", cacheid, "diff")}`
+  await $`chmod 755 ${path.join(baseOutPath, "overlay2", cacheid, "diff")}`
+  if(key > 0) {
+    await $`echo ${linkIdFullChain.slice(0, key).join(":")} > ${path.join(baseOutPath, "overlay2", cacheid, "lower")}`
+    await $`chmod 644 ${path.join(baseOutPath, "overlay2", cacheid, "lower")}`
+  }
+  
+  await $`ln -s ${path.join('..', cacheid, "diff")} ${path.join(baseOutPath, "overlay2", "l", linkid)}`
 }
 
 // create the repositories.json snippet for the image
