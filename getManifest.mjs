cdaba7440ccd144a8161e8b38c8e34c23a0bb905
zoobot <rommims@gmail.com>
2022-07-29 13:21:52 -0700

    works

--- getManifest.mjs
+++ getManifest.mjs
@@ -4,27 +4,30 @@ import axios from 'axios';
 import dockerParseImage from 'docker-parse-image';
 import { fs } from 'zx';
 import { inspect } from 'util';
+import * as stream from 'stream';
+import { promisify } from 'util';
+
+const finished = promisify(stream.finished);
 import path from 'path';
-import jwkToPem from 'jwk-to-pem';
-import mod_jws from 'jws';
+// import jwkToPem from 'jwk-to-pem';
+// import mod_jws from 'jws';
+// import fetch from 'node-fetch';
 
 /*
   This should authenticate to the registry api, get a token,
-  Get the manifest from the registry and get the blobs.
- 
-  TODO: work on functions getHeadBlob then getBlob
+  Get the distribution manifest, the manifest from the registry and get the blobs.
 
   How to run:
   npm i
   node getManifest.mjs
 
-  You can also replace scopeo with this but it's currently only getting the manifest, not the blobs.
+  You can also replace scopeo with this.
 */
 
 
 /* 
-Pull manifests using the endpoint /v2/<name>/manifests/<reference>
-Pull blobs using the endpoint /v2/<name>/blobs/<digest>
+** /v2/<name>/manifests/<reference>
+** /v2/<name>/blobs/<digest>
 
 Proof of Concept
 Used a reverse proxy to catch the HTTP requests made by the 
@@ -36,31 +39,23 @@ Notice how we get back the image Id on Docker-Content-Digest header, as specifie
 2. After validating that the manifest exists just requests a token for each of the layers and it's data: First layer
 */
 
-// https://docs.docker.com/registry/spec/api/
-// https://github.com/dlgmltjr0925/docker-registry-web/blob/cbab3f214d3d47be3c93d1b5ab969f7b711663fc/utils/dockerRegistry.ts
-// https://github.com/TritonDataCenter/node-docker-registry-client/blob/master/lib/registry-client-v2.js
-// https://github.com/moby/moby/issues/9015
-// https://github.com/containers/skopeo/blob/main/cmd/skopeo/copy.go
-// https://github.com/mafintosh/docker-parse-image/blob/master/index.js
-// https://gist.github.com/leodotcloud/9cd3dabdc73ccb498777073a0c8df64a
-// https://github.com/moby/moby/blob/0910306bf970603ce787466a98e4294ba81af841/layer/layer_store.go#L102
-// https://programmer.ink/think/container-principle-understand-layerid-diffid-chainid-cache-id.html
-// https://github.com/productionwentdown/dri/blob/e7a85c5666f45b716be47d112be2578638143fbf/src/api.js
-// https://github.com/viraja1/decentralized_docker_hub_registry/blob/782de6b84532c70c51049b3aec35a177998f089a/daemon/server.js
-// https://github.com/bmonty/docker-manifest
-// https://github.com/viraja1/decentralized_docker_hub_registry/blob/782de6b84532c70c51049b3aec35a177998f089a/hub/server.js
-// https://github.com/plurid/hypod/blob/c69c53ef8c9aa41741144b416d2109c55a5eb7e1/packages/hypod-server/source/server/data/constants/docker/index.ts
-
-
-async function fetchFromRegistry(url, options, callerFunction) {
-  try {
-    const response = await axios.get(url, options);
-    return await response;
-  } catch (error) {
-    // console.error('===> fetchFromRegistry', error, callerFunction);
-    // throw new Error('==> failed to fetch.');
-  }
-}
+/* 
+Helpful links to understand the registry and other versions of registry code:
+https://docs.docker.com/registry/spec/api/
+https://github.com/dlgmltjr0925/docker-registry-web/blob/cbab3f214d3d47be3c93d1b5ab969f7b711663fc/utils/dockerRegistry.ts
+https://github.com/TritonDataCenter/node-docker-registry-client/blob/master/lib/registry-client-v2.js
+https://github.com/moby/moby/issues/9015
+https://github.com/containers/skopeo/blob/main/cmd/skopeo/copy.go
+https://github.com/mafintosh/docker-parse-image/blob/master/index.js
+https://gist.github.com/leodotcloud/9cd3dabdc73ccb498777073a0c8df64a
+https://github.com/moby/moby/blob/0910306bf970603ce787466a98e4294ba81af841/layer/layer_store.go#L102
+https://programmer.ink/think/container-principle-understand-layerid-diffid-chainid-cache-id.html
+https://github.com/productionwentdown/dri/blob/e7a85c5666f45b716be47d112be2578638143fbf/src/api.js
+https://github.com/viraja1/decentralized_docker_hub_registry/blob/782de6b84532c70c51049b3aec35a177998f089a/daemon/server.js
+https://github.com/bmonty/docker-manifest
+https://github.com/viraja1/decentralized_docker_hub_registry/blob/782de6b84532c70c51049b3aec35a177998f089a/hub/server.js
+https://github.com/plurid/hypod/blob/c69c53ef8c9aa41741144b416d2109c55a5eb7e1/packages/hypod-server/source/server/data/constants/docker/index.ts
+*/ 
 
 function getRegistryUrl(image) {
   const parsedImage = dockerParseImage(image);
@@ -68,20 +63,61 @@ function getRegistryUrl(image) {
   return 'https://registry2.balena-cloud.com/v2/';
 }
 
-
-
-function createAuthTokenHeaderOptions(token) {
+function createManifestOptions(token) {
   return {
       "headers": {
           "Accept": "application/vnd.docker.distribution.manifest.v2+json",
-          "Content-Type": "application/vnd.docker.distribution.manifest.v1+json",
           "Authorization": `Bearer ${token}`
       }}}
+
+
+/* TODO: iterate through all blobs, compare sizes, and download the ones that are missing
+** /v2/<name>/blobs/<digest>
+** https://stackoverflow.com/questions/71534322/http-stream-using-axios-node-js
+*/
+async function getAllBlobs(image, token, manifest, baseInPath) {
+  
+  const options = {
+    "method": "GET",
+    "headers": {
+      "Authorization": `Bearer ${token}`,
+      "Accept": "application/vnd.docker.image.rootfs.diff.tar.gzip",
+      "Accept-Encoding": "gzip",
+      "responseType": 'stream',
+    },
+  }
+   const headCache = {};
+   const host = 'https://registry2.77105551e3a8a66011f16b1fe82bc504.bob.local/v2/v2/53b00bed7a4c6897db23eb0e4cf620e3'
+    const tgzLayersDigest = await Promise.all(manifest.layers.map(async (layer) => {
+
+      console.log('\n\n==> getAllBlob response layer', inspect(await layer, true, 2, true))
+      const writer = fs.createWriteStream(`${baseInPath}/${layer.digest.split(':')[1]}`);
+      options.url = `${host}/blobs/${layer.digest}`;
+      const { data, headers } = await axios(options)
+      // if (!(data instanceof stream)) console.log('Not stream');
+      console.log('\n\n==> getAllBlob response layer', inspect(await layer, true, 2, true))
+      console.log('\n\n==> getAllBlob response headers', inspect(await headers, true, 2, true))
+      // console.log('\n\n==> getAllBlob response data', inspect(await data, true, 10, true))
+      if ((headers['content-length'] === layer.size) 
+        && (headers['docker-content-digest'] === layer.digest)) {
+          writer.write(data);
+          writer.end();
+        }
+
+      // if (await headers.status === 404) {
+      //   console.error('==> blob not found', layer.digest);
+      //   headCache[layer.digest] = 'failed';
+      //   return headCache
+      // }
+      headCache[layer.digest] = 'success';
+      return headCache
+    }))
+}
 
 // TODO Finish getBlobs
 // GET /v2/<name>/blobs/<digest></digest>
 // This should pull the blob from the registry after checking head.
-async function getBlobs(image, token, configDigest, contentLength) {
+async function getBlobs(image, token, configDigest, contentLength,baseInPath) {
   const options = {
     "method": "GET",
     "headers": {
@@ -100,8 +136,10 @@ async function getBlobs(image, token, configDigest, contentLength) {
   try {
     const res = await axios.get(url, options);
     console.log('==> getBlob res.data', await res.data)
-    fs.writeFileSync(`${baseInPath}/${configDigestName}`, JSON.stringify(await res.data, null, 2));
+    fs.writeFileSync(`${baseInPath}/${configDigest}`, JSON.stringify(await res.data, null, 2));
     console.log('==> getBlob res.headers', await res.headers)
+
+    
   } catch (error) {
     console.error('==> getBlob error', error)
     // throw new Error(`\n\n==> getBlob => ERROR: ${error}`);
@@ -184,13 +222,13 @@ async function getHeadBlob(image, token, manifest,configDigest,fsLayers) {
 // passing in Tag (manifest.config.digest) will get the config.digest.
 // https://{registry}/v2/{imageName}/manifests/{tag}
 async function getConfig(image, token, manifest, baseInPath) {
-  const options = createAuthTokenHeaderOptions(token);
-  options.headers.Accept = "application/vnd.docker.container.image.v1+json";
+  const options = createManifestOptions(token);
+  options.headers.Accept = "application/vnd.docker.container.image.v2+json";
   const url = makeManifestUrl(image, manifest.config.digest);
   const configDigestName = manifest.config.digest.split(":")[1];
   try {
     const res = await axios.get(url, options);
-    // fs.writeFileSync(`${baseInPath}/${configDigestName}`, JSON.stringify(await res.data, null, 2));
+    fs.writeFileSync(`${baseInPath}/${configDigestName}`, JSON.stringify(await res.data, null, 2));
     console.log(await res.headers, '==> getConfig res.headers' )
     console.log(await res.data, '==> getConfig res.data')
     console.log(await inspect(res.data.signatures[0].header), '==> getConfig res.data.signatures[0].header')
@@ -202,74 +240,56 @@ async function getConfig(image, token, manifest, baseInPath) {
   }
 }
 
+/**
+ * GET /v2/<name>/manifests/<reference>
+ * Host: <registry host>
+ * Authorization: <scheme> <token>
+ */
 function makeManifestUrl(image) {
   // registry spec https://docs.docker.com/registry/spec/api/
   const baseRegistry = getRegistryUrl(image);
   const parsedImage = dockerParseImage(image);
   const {repository, registry, tag, namespace} = parsedImage;
-  const manifestUrl = `${baseRegistry}/${namespace || ''}/${repository}/manifests/${tag || 'latest'}`;
+  const manifestUrl = `${baseRegistry}${namespace || ''}/${repository}/manifests/${tag || 'latest'}`;
   return manifestUrl;
 }
 
-/**
- * GET /v2/<name>/manifests/<reference>
- * Host: <registry host>
- * Authorization: <scheme> <token>
- */
-async function getManifest(image, token, options = {}) {
-  const optionsManifest = createAuthTokenHeaderOptions(token, options);
-  const url = makeManifestUrl(image);
+async function getManifest(image, token, authHeaders,baseInPath) {
+  console.log(token, '==> getManifest token');
+  const options = {
+    "method": "GET",
+    withCredentials: true,
+    credentials: 'include',
+    "headers": {
+      "Accept": 'application/vnd.docker.distribution.manifest.v2+json',
+      "Authorization": `Bearer ${token}`,
+    },
+  };
+  console.log(options, '==> getManifest optionsManifest');
+  // const url = makeManifestUrl(image);
+  const host = 'https://registry2.77105551e3a8a66011f16b1fe82bc504.bob.local/v2/v2/53b00bed7a4c6897db23eb0e4cf620e3'
+  const url = `${ host}/manifests/latest`;
+  console.log(url, '==> getManifest url');
   try {
-    const res = await axios.get(url, optionsManifest);
-    // console.log('\n\n\n ==> 1 getManifest res headers', inspect(await res.headers, true, 10, true))
-    // console.log('==> 1 getManifest res data', inspect(await res.data, true, 10, true))
-    
-    // optionsManifest.headers.Accept =
-    //       "application/vnd.docker.distribution.manifest.v2+json";
-    // const res2 = await axios.get(url, options);
+    const res = await axios.get(url, options);
+    const data = await res.data;
+    console.log(inspect(await data,true,10,true), '==> getManifest res');
     const digest = res.headers["docker-content-digest"];
-
-    // console.log('\n\n\n ==> 1 getManifest digest', inspect(digest, true, 10, true))
-    // console.log('\n\n\n ==> 2 getManifest digest', inspect(res.data, true, 10, true))
-    fs.writeFileSync(`${baseInPath}/manifest.json`, JSON.stringify(await res.data, null, 2));
-    // return await res.data;
-    return { ...res.data, digest };
-
+    console.log(digest, '==> getManifest digest');
+    console.log(inspect(baseInPath,true,10,true), '==> getManifest baseInPath');
+    fs.writeFileSync(`${baseInPath}/manifest.json`, JSON.stringify(await data, null, 2));
+    return { ...data, digest };
   } catch (error) {
-    throw new Error(`==> Nope did not get registry manifest data. ERROR: ${error}`);
+    console.error(inspect(error,true,10,true), '==> getManifest error');
+    // throw new Error(`==> NOPE did not get registry manifest data. ERROR: ${error}`);
   }
 }
-
-/**
- * GET /v2/<name>/manifests/<reference>
- * Host: <registry host>
- * Authorization: <scheme> <token>
- */
-//  export const getManifests = async ({
-//   host,
-//   authorization,
-//   name,
-//   reference,
-// }: GetManifestsArgs): Promise<GetManifestsResponse | undefined> => {
-//   try {
-//     const configs: AxiosRequestConfig = {};
-//     if (authorization) configs.headers = { Authorization: authorization };
-//     const url = getRegistyUrl(host, `/${name}/manifests/${reference}`);
-//     const res1 = await axios.get<Manifest>(url, configs);
-//     configs.headers.Accept =
-//       "application/vnd.docker.distribution.manifest.v2+json";
-//     const res2 = await axios.get<DistributionManifest>(url, configs);
-//     const digest = res2.headers["docker-content-digest"] as string;
-//     return { ...res1.data, digest };
-//   } catch (error) {
-//     throw handleError(error);
-//   }
-// };
 
 const  getToken = async (image, options, authResponse, tag) => {
   try {
     const parsedImage = dockerParseImage(image);
     const tokenOptions = {
+      method: 'GET',
       params: {
         service: authResponse.service,
         scope: `repository:${parsedImage.namespace 
@@ -279,8 +299,9 @@ const  getToken = async (image, options, authResponse, tag) => {
       ...options,
     };
     console.log('\n\n\n ==> getToken tokenOptions', inspect(tokenOptions, true, 10, true))
-    const tokenResponse = await fetchFromRegistry(authResponse.realm, tokenOptions, 'getToken');
+    const tokenResponse = await axios.get(authResponse.realm, tokenOptions);
     if (!tokenResponse.data.token) throw new Error("token registry fail.");
+    console.log(typeof tokenResponse.data.token,'==> getToken tokenResponse.data.token');
     return await tokenResponse.data.token;
   } catch (error) {
     console.log(error);
@@ -329,7 +350,7 @@ async function getRealmResponse(image, options) {
   }
 }
 
-const getAuthHeaders = async (options) => {
+function getAuthHeaders(options) {
   return {
     auth: {
         username: options?.user,
@@ -354,7 +375,10 @@ const getAuthHeaders = async (options) => {
     
 }
 
+
 export const pullManifestFromRegistry = async (image, userInfo, baseInPath) => {
+
+  
   const authHeaders = getAuthHeaders(userInfo);
   console.log('\n\n==> authHeaders', authHeaders);
   const authResponseForRealm = await getRealmResponse(image, authHeaders);
@@ -363,8 +387,9 @@ export const pullManifestFromRegistry = async (image, userInfo, baseInPath) => {
   console.log('\n\n==> token', token);
   console.log('\n\n==> HERE  manifest' ); 
   const manifest = await getManifest(image, await token, authHeaders, baseInPath);
+  
+  console.log(manifest,'==> HERE  manifest manifest \n\n', ); 
   const configDigest = manifest.config.digest;
-  // console.log(configDigest,'==> HERE  manifest configDigest \n\n', ); 
   console.log(manifest.config,'==> HERE  manifest manifest.config \n\n', ); 
   const digests = manifest.layers.map(layer => layer.digest);
   // console.log(digests, '==> HERE  manifest digests\n\n'); 
@@ -372,18 +397,20 @@ export const pullManifestFromRegistry = async (image, userInfo, baseInPath) => {
   const config = await getConfig(image, await token, manifest, baseInPath);
   const fsLayers = config.fsLayers.map(fslayer => fslayer.blobSum);
   // console.log(inspect(fsLayers,true,10,true), '==> config fsLayers \n\n'); 
-  const layerArray = [configDigest, manifest.digest, ...fsLayers,  ...digests];
-  // blob = fetch_blob(docker_image, auth, manifest)
   const contentLength = await getHeadBlob(image, await token, manifest, configDigest, fsLayers);
   console.log(contentLength,'=====> contentLength')
-  const layers = await getBlobs(image, await token, configDigest, contentLength);
+  const layers = await getBlobs(image, await token, configDigest, contentLength, baseInPath);
   console.log(layers,'=====> layers')
+  const allBlobAlltheTime = await getAllBlobs(image, await token, manifest, baseInPath);
+  console.log(allBlobAlltheTime,'=====> allBlobAlltheTime');
+  console.log( image,'image HERE ==> \n\n');
   return manifest;
 }
 
-
 const image = 'registry2.77105551e3a8a66011f16b1fe82bc504.bob.local/v2/53b00bed7a4c6897db23eb0e4cf620e3'
-const baseInPath = './test'
+// const baseInPath = './test'
+const baseInPath = `in/images/${image.split("/").reverse()[0]}`
+// const baseOutPath = path.join(__dirname, 'out', 'docker')
 // commitHash = sha256:ab662eecdb30b71c7658e8c7d40eca4ae4bcde9eac117ed0022fc522da23a86e
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJib2IiLCJlbWFpbCI6ImJvYkBiYWxlbmEubG9jYWwiLCJjcmVhdGVkX2F0IjoiMjAyMi0wNi0wMlQyMjozMTo0MC41ODFaIiwiand0X3NlY3JldCI6IkVRWDdOS1FZT1BHRFRNUFRDUjc3RUxOSU9XQVpUWVVNIiwiaGFzX2Rpc2FibGVkX25ld3NsZXR0ZXIiOnRydWUsImZpcnN0X25hbWUiOiIiLCJsYXN0X25hbWUiOiIiLCJhY2NvdW50X3R5cGUiOiIiLCJzb2NpYWxfc2VydmljZV9hY2NvdW50IjpbXSwiY29tcGFueSI6IiIsImhhc1Bhc3N3b3JkU2V0Ijp0cnVlLCJwdWJsaWNfa2V5Ijp0cnVlLCJmZWF0dXJlcyI6WyJub3RpZmljYXRpb25zIl0sImludGVyY29tVXNlck5hbWUiOiJbNzcxMDU1NTFFM0E4QTY2MDExRjE2QjFGRTgyQkM1MDRdIGJvYiIsInBlcm1pc3Npb25zIjpbImFkbWluLmhvbWUiLCJhZG1pbi5sb2dpbl9hc191c2VyIl0sImF1dGhUaW1lIjoxNjU4NjY4MjkxMjUxLCJpYXQiOjE2NTg2NjgyOTEsImV4cCI6MTY1OTI3MzA5MX0.2DIcV_c5dBMYVaxrRGSiflGrVfh6F3x1R56kMOZ19Xw'
 const userInfo = {
@@ -391,4 +418,5 @@ const userInfo = {
   token,
   // await fs.readFileSync('~/.balena/token', 'utf8')
 }
+
 pullManifestFromRegistry(image, userInfo, baseInPath)
