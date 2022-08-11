fbbe9f118026dd59cd0023c7c11964ad419ee151
zoobot <rommims@gmail.com>
2022-08-11 14:25:45 -0700

    removed root level folders

---
+++ logger.mjs
@@ -0,0 +1,114 @@
+import winston from 'winston';
+import { format } from 'logform';
+
+const consoleLevel = 'debug';
+
+const warnLevels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    debug: 4,
+  },
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'orange',
+    verbose: 'blue',
+    debug: 'pink',
+  },
+};
+
+// log to file everything over consoleLevel
+const logger = winston.createLogger({
+  level: consoleLevel,
+  levels: warnLevels.levels,
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.json(),
+  ),
+  transports: [
+    new winston.transports.File({
+      timestamp: true,
+      colorize: true,
+      filename: './logs/error.log',
+      level: 'error',
+      handleExceptions: true,
+      json: true,
+      maxsize: 52428800,
+      maxFiles: 1,
+    }),
+    new winston.transports.File({
+      timestamp: true,
+      colorize: true,
+      filename: './logs/warn.log',
+      level: 'warn',
+      handleExceptions: true,
+      json: true,
+      maxsize: 52428800,
+      maxFiles: 1,
+    }),
+    new winston.transports.File({
+      timestamp: true,
+      colorize: true,
+      filename: './logs/info.log',
+      level: 'info',
+      handleExceptions: true,
+      json: true,
+      maxsize: 52428800,
+      maxFiles: 1,
+    }),
+    new winston.transports.File({
+      timestamp: true,
+      colorize: true,
+      filename: './logs/verbose.log',
+      level: 'verbose',
+      handleExceptions: true,
+      json: true,
+      maxsize: 52428800,
+      maxFiles: 1,
+    }),
+    new winston.transports.File({
+      timestamp: true,
+      colorize: true,
+      filename: './logs/debug.log',
+      level: 'debug',
+      handleExceptions: true,
+      json: true,
+      maxsize: 52428800,
+      maxFiles: 1,
+    }),
+    new winston.transports.File({
+      timestamp: true,
+      colorize: true,
+      filename: './logs/combined.log',
+      handleExceptions: true,
+      json: true,
+      maxsize: 52428800,
+      maxFiles: 1,
+    }),
+    new winston.transports.Console({
+      level: consoleLevel,
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+      timestamp: true,
+      format: format.combine(
+        format.colorize({ all: true }),
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+        ),
+      ),
+    }),
+  ],
+});
+
+export const stream = {
+  write(message, encoding) {
+    logger.info(`${message}, ${encoding}`);
+  },
+};
+
+export default logger
--- main.mjs
+++ main.mjs
@@ -1,18 +1,12 @@
 import streamPreloadingAssets from "./lib/streamPreloadingAssets.mjs";
 import path from "path"
 import fs from "fs-extra"
+import { getEnvs } from "./lib/getAuth.mjs";
 import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-// variables
-const app_id = "7ea7c15b12144d1089dd20645763f790" // "ed91bdb088b54a3b999576679281520a" ee6c3b3f75ae456d9760171a27a36568
-const release_id = "302261f9d08a388e36deccedac6cb424" // "2f24cd2be3006b029911a3d0da6837d5" 
-const balenaosRef = "expanded-aarch64"
-const user = "edwin3"
-const password = process.env.PASSWD
-
-if (!password) throw new error("Password is missing, launch this with `PASSWD=****** node streaming.mjs`")
+  
+const { app_id, release_id, balenaosRef, user, password } = getEnvs()
 
 // TODO: output is currently a file, it should become the response of a http request
 const outputStream = fs.createWriteStream(path.join(__dirname, "out", "tarball.tar"))
