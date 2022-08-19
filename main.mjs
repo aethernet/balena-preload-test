6f93ef5b621825513d6a91ce1568e804f28724d6
JOASSART Edwin <edwin.joassart@ext.europarl.europa.eu>
2022-08-19 11:10:17 +0200

    - change assets inject folder to match inject.mjs expectations - add apps.json in inject assets

--- main.mjs
+++ main.mjs
@@ -8,13 +8,11 @@ const balenaosRef = process.env.BALENAOS
 const user = process.env.USER
 const password = process.env.PASSWORD
 const tarball = process.env.TARBALL
+const dataPartition = process.env.DATA_PARTITION
 
 // TODO: output is currently a file, it should become the response of a http request
 const outputStream = fs.createWriteStream(tarball)
 
-// subFolder in the tar archive where we keep the injectables
-const injectFolder = "inject/resin-data"
-
-await streamPreloadingAssets({ outputStream, user, password, app_id, release_id, balenaosRef, injectFolder })
+await streamPreloadingAssets({ outputStream, user, password, app_id, release_id, balenaosRef, dataPartition })
 
 console.log(`=== Your tarball is ready : ${tarball} ===`)
