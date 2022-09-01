8212a34c60907c25ca0572a8dec5a766921f4703
Edwin Joassart <edwin@blck.coffee>
2022-09-01 16:23:16 +0200

    more refactoring to prepare for integration in image maker

--- main.mjs
+++ main.mjs
@@ -1,6 +1,6 @@
 import streamPreloadingAssets from "./lib/streamPreloadingAssets.mjs"
-import fs from "fs-extra"
 import "dotenv/config"
+import fs from "fs-extra"
 
 const app_id = process.env.APPID
 const release_id = process.env.RELEASEID
@@ -9,10 +9,38 @@ const user = process.env.USER
 const password = process.env.PASSWORD
 const tarball = process.env.TARBALL
 const dataPartition = process.env.DATA_PARTITION
+const supervisorVersion = process.env.SV_VERSION
+const arch = process.env.ARCH
+const baseImage = process.env.BASEIMAGE
 
-// TODO: output is currently a file, it should become the response of a http request
+/**
+ * Get balenaos size
+ * @returns image size
+ */
+const getImageSize = (filePath) => {
+  const { size } = fs.statSync(filePath)
+  return size
+}
+
 const outputStream = fs.createWriteStream(tarball)
 
-await streamPreloadingAssets({ outputStream, user, password, app_id, release_id, balenaosRef, dataPartition })
+const balenaosStream = fs.createReadStream(baseImage)
+const balenaosSize = getImageSize(baseImage)
+
+balenaosStream.on("open", async () => {
+  await streamPreloadingAssets({
+    outputStream,
+    balenaosStream,
+    balenaosSize,
+    user,
+    supervisorVersion,
+    arch,
+    password,
+    app_id,
+    release_id,
+    balenaosRef,
+    dataPartition,
+  })
+})
 
 console.log(`=== Your tarball is ready : ${tarball} ===`)
