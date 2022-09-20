a62c00e0efe5970a657621afbd35368502e88a2c
zoobot <rommims@gmail.com>
2022-09-20 14:51:42 -0700

    test start

---
+++ tests/registry.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import * as path from 'path';
+
+describe('registry', function () {
+	describe('registry test1', function () {
+		it('should have ', function () {
+			expect('test1').to.deep.equal([]);
+		});
+    });
+});
