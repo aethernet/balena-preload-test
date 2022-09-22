4587e9227f0074731ee90b4635bcf772f2621df1
Edwin Joassart <edwin@blck.coffee>
2022-09-22 20:49:16 +0200

    store os with correct name and extension

--- lib/streamPreloadingAssets.ts
+++ lib/streamPreloadingAssets.ts
@@ -75,11 +75,12 @@ const streamPreloadingAssets = async ({
   // Beware that knowing the file size in advance is mandatory
   const baseImageStreamEntry = packStream.entry({
     // TOOD: name: `${balenaosRef}.img`, // switch when inject.mjs select baseimage from manifest (currently hardcoded)
-    name: "image.img",
+    name: `${balenaosRef}.gz`,
     mode: 644,
     size: balenaosSize,
   })
 
+  // TODO: optimizatinon : // streamBaseImage with all the metadata retrieval and processing (up to getLayers)
   await streamBaseImage({ pipeStreamFrom: balenaosStream, pipeStreamTo: baseImageStreamEntry })
 
   // get apps.json
